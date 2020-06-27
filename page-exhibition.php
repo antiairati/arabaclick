0b66deb95eb31c7720e26a834f9620607e472a27
antiairati <antiairati@gmail.com>
2020-06-28 00:07:35 +0200

    next edition child

--- page-exhibition.php
+++ page-exhibition.php
@@ -5,13 +5,38 @@ Template Name: Page Exhibition
 ?>
 
 <?php get_header() ?>
-<div class="container" style="margin-top:100px;">
-  <div class="row">
-    <div class="col-lg-2">
-      <div class="" style="background-color:green;">
-        <h1>This is the exhibitions page!</h1>
+<section class="f--section-primary">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8">
+        <h1 class="f--title f--font-secondary">Exposiciones</h1>
+        <h2 class="f--tagline f--font-fourth">#observa</h2>
       </div>
     </div>
   </div>
-</div>
+</section>
+<section class="f--section-third">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-7">
+        <div class="b--content">
+          <p>La Asociación Kikute para el fomento de actividades deportivas, culturales y tecnológicas (kirola, kultura eta teknologia), es una asociación sin ánimo de lucro orientada a la creación de eventos en el ámbito del Territorio Histórico de Araba y, en este caso propone, la creación de un evento en el ámbito de la fotografía con dispositivo móvil.</p>
+          <br>
+          <p>El evento lo hemos denominado Arabaclick. Un territorio, un gesto y un sonido asociado a una imagen. Detrás de cada click hay una historia y creemos que esta es la mejor forma de conocer nuestro entorno. A través de sus gentes podemos disfrutar de todo el territorio alavés en un click.</p>
+          <br>
+          <p>Para poder alcanzar un evento global nos hemos aliado con entidades y personas de referencia con diferentes tendencias en tecnología, el arte y la difusión en redes sociales y así lograr un marco en el que se puedan mejorar aptitudes y ponerlas en práctica. Contamos con la colaboración de ID arte, con la Escuela de Artes y Oficios, con la Fundación Sancho el Sabio y con la Asociación de Fotógrafos de Gasteiz-Fotogasteiz. (¿Alava visión?)</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="f--section-third">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-10 offset-lg-1">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2925.483866068533!2d-2.6748412844503062!3d42.84151527915753!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd4fc271ec423353%3A0xb80eebb3710f0192!2sVitoria%20-%20Gasteiz!5e0!3m2!1ses!2ses!4v1593295467768!5m2!1ses!2ses" width="100%" height="400px" frameborder="0" style="border:0px;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+      </div>
+    </div>
+  </div>
+</section>
 <?php get_footer() ?>
--- page-open-call.php
+++ page-open-call.php
@@ -5,13 +5,29 @@ Template Name: Page Open Call
 ?>
 
 <?php get_header() ?>
-<div class="container" style="margin-top:100px;">
-  <div class="row">
-    <div class="col-lg-2">
-      <div class="" style="background-color:green;">
-        <h1>This is the open call page!</h1>
+<section class="f--section-primary">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8">
+        <h1 class="f--title f--font-secondary">Convocatorias</h1>
+        <h2 class="f--tagline f--font-fourth">#participa</h2>
       </div>
     </div>
   </div>
-</div>
+</section>
+<section class="f--section-third">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-7">
+        <div class="b--content">
+          <p>La Asociación Kikute para el fomento de actividades deportivas, culturales y tecnológicas (kirola, kultura eta teknologia), es una asociación sin ánimo de lucro orientada a la creación de eventos en el ámbito del Territorio Histórico de Araba y, en este caso propone, la creación de un evento en el ámbito de la fotografía con dispositivo móvil.</p>
+          <br>
+          <p>El evento lo hemos denominado Arabaclick. Un territorio, un gesto y un sonido asociado a una imagen. Detrás de cada click hay una historia y creemos que esta es la mejor forma de conocer nuestro entorno. A través de sus gentes podemos disfrutar de todo el territorio alavés en un click.</p>
+          <br>
+          <p>Para poder alcanzar un evento global nos hemos aliado con entidades y personas de referencia con diferentes tendencias en tecnología, el arte y la difusión en redes sociales y así lograr un marco en el que se puedan mejorar aptitudes y ponerlas en práctica. Contamos con la colaboración de ID arte, con la Escuela de Artes y Oficios, con la Fundación Sancho el Sabio y con la Asociación de Fotógrafos de Gasteiz-Fotogasteiz. (¿Alava visión?)</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 <?php get_footer() ?>
