0b9ddf51334024a0bf6c631ef7a1cc5785c2d37d
antiairati <antiairati@gmail.com>
2020-06-27 22:39:03 +0200

    team page

--- page-team.php
+++ page-team.php
@@ -5,13 +5,51 @@ Template Name: Page Team
 ?>
 
 <?php get_header() ?>
-<div class="container" style="margin-top:100px;">
-  <div class="row">
-    <div class="col-lg-2">
-      <div class="" style="background-color:green;">
-        <h1>This is the team page!</h1>
+<section class="f--section-primary">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8">
+        <h1 class="f--title f--font-secondary">Equipo</h1>
+        <h2 class="f--tagline f--font-fourth">Un grupo de gente apasionada de la fotografía</h2>
       </div>
     </div>
   </div>
-</div>
+</section>
+<section class="f--section-third">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-7">
+        <div class="b--content">
+          <p>La Asociación Kikute para el fomento de actividades deportivas, culturales y tecnológicas (kirola, kultura eta teknologia), es una asociación sin ánimo de lucro orientada a la creación de eventos en el ámbito del Territorio Histórico de Araba y, en este caso propone, la creación de un evento en el ámbito de la fotografía con dispositivo móvil.</p>
+          <br>
+          <p>El evento lo hemos denominado Arabaclick. Un territorio, un gesto y un sonido asociado a una imagen. Detrás de cada click hay una historia y creemos que esta es la mejor forma de conocer nuestro entorno. A través de sus gentes podemos disfrutar de todo el territorio alavés en un click.</p>
+          <br>
+          <p>Para poder alcanzar un evento global nos hemos aliado con entidades y personas de referencia con diferentes tendencias en tecnología, el arte y la difusión en redes sociales y así lograr un marco en el que se puedan mejorar aptitudes y ponerlas en práctica. Contamos con la colaboración de ID arte, con la Escuela de Artes y Oficios, con la Fundación Sancho el Sabio y con la Asociación de Fotógrafos de Gasteiz-Fotogasteiz. (¿Alava visión?)</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="f--section-third">
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <div class="b--logo-wrapper">
+          <div class="b--logo-wrapper__item">
+            <img class="b--logo-wrapper__item__img" src="https://lh3.googleusercontent.com/proxy/N5lyRuAYbaA89Uxq8-Hvm7LOpCWcyDVIaFiwuGr0rQVwIzwlnWPmjCUNVp04gMp1tL9kd8rriQ2H-pvMJ5tpO230F__hqdMwLMPrAd2AwkqFLKPfQ6UkRw" alt="">
+          </div>
+          <div class="b--logo-wrapper__item">
+            <img class="b--logo-wrapper__item__img" src="https://avoz.info/wp-content/uploads/2016/10/escuela-artes-oficios-vitoria-gasteiz.png" alt="">
+          </div>
+          <div class="b--logo-wrapper__item">
+            <img class="b--logo-wrapper__item__img" src="https://www.sanchoelsabio.eus/wp-content/uploads/2015/06/logosanchoelsabio.png" alt="">
+          </div>
+          <div class="b--logo-wrapper__item">
+            <img class="b--logo-wrapper__item__img" src="https://fotogasteiz.com/vitorianos/wp-content/uploads/2019/02/logo-asociacio%CC%81n-fotogasteiz-blanco-800x800.jpg" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 <?php get_footer() ?>
