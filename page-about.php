0809544156a49cf0d6f32916a5bfbe5140e567fc
antiairati <antiairati@gmail.com>
2020-06-27 21:16:14 +0200

    page-about

--- page-about.php
+++ page-about.php
@@ -5,13 +5,61 @@ Template Name: Page About
 ?>
 
 <?php get_header() ?>
-<div class="container" style="margin-top:100px;">
-  <div class="row">
-    <div class="col-lg-8">
-      <div class="">
-        <h1 class="b--font-primary">Quiénes somos</h1>
+<section class="f--section-primary">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <h1 class="f--title f--font-primary">Quiénes somos</h1>
+        <h2 class="f--tagline f--font-fourth">Fotografía y móvil</h2>
       </div>
     </div>
   </div>
-</div>
+</section>
+<section class="f--section-third position-relative">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-5 offset-lg-1">
+        <div class="position-sticky b--content">
+          <p>Hemos pasado de lo analógico a lo digital y “la nube” esta cargada de imágenes susceptibles de ser transformadas en numerosos formatos. Platos, libros, alfombras, tazas, cuadros, puzzles, cojines, camisetas… La imaginación es “impresionante”.</p>
+          <br>
+          <p>Todas las personas con un teléfono móvil son fotógrafas que mediante un simple gesto cuentan historias en las redes sociales y las llenan de imágenes de diferentes temáticas: Naturaleza, arquitectura, eventos culturales y deportivos, personas, moda, gastronomía, arte, diseño…</p>
+        </div>
+      </div>
+      <div class="col-lg-5 offset-lg-1">
+        <div class="f--img-wrapper">
+          <img class="f--img-wrapper__img" src="https://cdn5.eyeem.com/thumb/9c75c8f204e2dfbc6ab2bfd233b4bb1fa780ef29-1554814708048/w/1280" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="f--section-third">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6 offset-lg-3 text-center">
+        <h3 class="f--font-third f--color-primary text-center f--tagline">“Lo más importante no es la cámara, sino el ojo.”</h3>
+        <span class="f--font-fourth f--color-primary text-center"> - Alfred Eisenstaedt - </span>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="f--section-third position-relative">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-5">
+        <div class="f--img-wrapper">
+          <img class="f--img-wrapper__img" src="https://cdn2.eyeem.com/thumb/38814c0505fcdce0cf620d72412a5c57eb99b85b-1552061001723/w/1300" alt="">
+        </div>
+      </div>
+      <div class="col-lg-5 offset-lg-1">
+        <div class="position-sticky b--content">
+          <p>Antes de hacer un viaje, de ir a un restaurante, de comprar un producto, de ver un evento cultural, etc. acudimos a internet en busca de información y lo que más nos impacta son las imágenes. Si nos agradan, tendremos una actitud proactiva y no dudaremos en el momento de decidirnos.</p>
+          <br>
+          <p>Esta es la importancia de la fotografía. Todo lo que nos imaginamos está fotografiado y si estas fotografías transmiten belleza la transformamos inmediatamente en un deseo por conocer, y como no puede ser de otra forma, de fotografiar siendo partícipes del escenario.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
 <?php get_footer() ?>
