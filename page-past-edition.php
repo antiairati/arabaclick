d9c84bde49ce852c4e85ae8ab8fdba0b9ac2081d
antiairati <antiairati@gmail.com>
2020-06-27 23:04:55 +0200

    past edition

--- page-past-edition.php
+++ page-past-edition.php
@@ -5,13 +5,59 @@ Template Name: Page Past Edition
 ?>
 
 <?php get_header() ?>
-<div class="container" style="margin-top:100px;">
-  <div class="row">
-    <div class="col-lg-2">
-      <div class="" style="background-color:green;">
-        <h1>This is the next edition page!</h1>
+<section class="f--section-primary">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <h1 class="f--title f--font-primary f--color-primary">Ediciones anteriores</h1>
+        <h2 class="f--tagline f--font-fourth f--color-primary">Disfrutando con la fotografía con móvil en Vitoria desde 2019</h2>
       </div>
     </div>
   </div>
-</div>
+</section>
+<section class="f--section-third position-relative">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-4">
+        <div class="position-sticky b--content f--color-primary">
+          <h3>2019</h3>
+          <br>
+          <p>Hemos pasado de lo analógico a lo digital y “la nube” esta cargada de imágenes susceptibles de ser transformadas en numerosos formatos. Platos, libros, alfombras, tazas, cuadros, puzzles, cojines, camisetas… La imaginación es “impresionante”.</p>
+          <br>
+          <p>Todas las personas con un teléfono móvil son fotógrafas que mediante un simple gesto cuentan historias en las redes sociales y las llenan de imágenes de diferentes temáticas: Naturaleza, arquitectura, eventos culturales y deportivos, personas, moda, gastronomía, arte, diseño…</p>
+        </div>
+      </div>
+      <div class="col-lg-5 offset-lg-1">
+        <div class="f--img-wrapper">
+          <img class="f--img-wrapper__img" src="https://ideolab.com/file/2019/06/keynote1.jpg" alt="">
+        </div>
+        <span class="f--font-sixth f--color-primary f--credit">© Ideolab</span>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="f--section-third position-relative">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-4">
+        <div class="f--img-wrapper">
+          <img class="f--img-wrapper__img" src="https://ideolab.com/file/2019/06/arabaclick_expo1-1116x744.jpg" alt="">
+        </div>
+        <span class="f--font-sixth f--color-primary f--credit">© Ideolab</span>
+      </div>
+      <div class="col-lg-4">
+        <div class="f--img-wrapper">
+          <img class="f--img-wrapper__img" src="https://pbs.twimg.com/media/D8xYp1EXoAAxUnI.jpg" alt="">
+        </div>
+        <span class="f--font-sixth f--color-primary f--credit">© Twitter</span>
+      </div>
+      <div class="col-lg-4">
+        <div class="f--img-wrapper">
+          <img class="f--img-wrapper__img" src="https://ideolab.com/file/2019/06/mockup_cartel.jpg" alt="">
+        </div>
+        <span class="f--font-sixth f--color-primary f--credit">© Ideolab</span>
+      </div>
+    </div>
+  </div>
+</section>
 <?php get_footer() ?>
