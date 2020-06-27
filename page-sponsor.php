d008bcf91423d634153da578d1ec7f8ee80c5c2b
antiairati <antiairati@gmail.com>
2020-06-27 22:53:18 +0200

    footer & patrocinadores

--- page-sponsor.php
+++ page-sponsor.php
@@ -5,13 +5,59 @@ Template Name: Page Sponsor
 ?>
 
 <?php get_header() ?>
-<div class="container" style="margin-top:100px;">
-  <div class="row">
-    <div class="col-lg-2">
-      <div class="" style="background-color:green;">
-        <h1>This is the sponsor page!</h1>
+<section class="f--section-primary">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8">
+        <h1 class="f--title f--font-secondary">Patrocinadores</h1>
+        <h2 class="f--tagline f--font-fourth">Gente que apoya el proyecto</h2>
       </div>
     </div>
   </div>
-</div>
+</section>
+<section class="f--section-third">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-7">
+        <div class="b--content">
+          <p>La Asociación Kikute para el fomento de actividades deportivas, culturales y tecnológicas (kirola, kultura eta teknologia), es una asociación sin ánimo de lucro orientada a la creación de eventos en el ámbito del Territorio Histórico de Araba y, en este caso propone, la creación de un evento en el ámbito de la fotografía con dispositivo móvil.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="f--section-third">
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <div class="b--logo-wrapper">
+          <div class="b--logo-wrapper__item">
+            <img class="b--logo-wrapper__item__img" src="https://lh3.googleusercontent.com/proxy/N5lyRuAYbaA89Uxq8-Hvm7LOpCWcyDVIaFiwuGr0rQVwIzwlnWPmjCUNVp04gMp1tL9kd8rriQ2H-pvMJ5tpO230F__hqdMwLMPrAd2AwkqFLKPfQ6UkRw" alt="">
+          </div>
+          <div class="b--logo-wrapper__item">
+            <img class="b--logo-wrapper__item__img" src="https://avoz.info/wp-content/uploads/2016/10/escuela-artes-oficios-vitoria-gasteiz.png" alt="">
+          </div>
+          <div class="b--logo-wrapper__item">
+            <img class="b--logo-wrapper__item__img" src="https://www.sanchoelsabio.eus/wp-content/uploads/2015/06/logosanchoelsabio.png" alt="">
+          </div>
+          <div class="b--logo-wrapper__item">
+            <img class="b--logo-wrapper__item__img" src="https://fotogasteiz.com/vitorianos/wp-content/uploads/2019/02/logo-asociacio%CC%81n-fotogasteiz-blanco-800x800.jpg" alt="">
+          </div>
+          <div class="b--logo-wrapper__item">
+            <img class="b--logo-wrapper__item__img" src="https://fotogasteiz.com/vitorianos/wp-content/uploads/2019/02/logo-asociacio%CC%81n-fotogasteiz-blanco-800x800.jpg" alt="">
+          </div>
+          <div class="b--logo-wrapper__item">
+            <img class="b--logo-wrapper__item__img" src="https://www.sanchoelsabio.eus/wp-content/uploads/2015/06/logosanchoelsabio.png" alt="">
+          </div>
+          <div class="b--logo-wrapper__item">
+            <img class="b--logo-wrapper__item__img" src="https://avoz.info/wp-content/uploads/2016/10/escuela-artes-oficios-vitoria-gasteiz.png" alt="">
+          </div>
+          <div class="b--logo-wrapper__item">
+            <img class="b--logo-wrapper__item__img" src="https://lh3.googleusercontent.com/proxy/N5lyRuAYbaA89Uxq8-Hvm7LOpCWcyDVIaFiwuGr0rQVwIzwlnWPmjCUNVp04gMp1tL9kd8rriQ2H-pvMJ5tpO230F__hqdMwLMPrAd2AwkqFLKPfQ6UkRw" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 <?php get_footer() ?>
